d8201c9baf9a2c7a076d7e525fbdc2b62a3cd92b
mertcan.aydin <canmert547@gmail.com>
2022-03-13 19:12:08 +0300

    .

---
+++ src/SecondDayE/CustomerManager.java
@@ -0,0 +1,14 @@
+package SecondDayE;
+
+public class CustomerManager {
+
+    public  void add(){
+        System.out.println("Müşteri eklendi.");
+    }
+    public  void update(){
+        System.out.println("Müşteri güncellendi.");
+    }
+    public void remove(){
+        System.out.println("Müşteri silindi.");
+    }
+}
---
+++ src/SecondDayE/Main.java
@@ -0,0 +1,15 @@
+package SecondDayE;
+
+public class Main {
+    public static void main(String[] args) {
+        CustomerManager customerManager = new CustomerManager();
+        customerManager.add();
+        customerManager.update();
+        customerManager.remove();
+
+        System.out.println("*****************************");
+
+
+
+    }
+}
